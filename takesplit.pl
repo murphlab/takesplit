0d1c00422bef3f392f6a9a18e77eee6c2a537f56
Ken Murphy <kenmrph@gmail.com>
2014-01-03 12:43:20 -0800

    read exif data

--- takesplit.pl
+++ takesplit.pl
@@ -12,6 +12,8 @@ BEGIN {
 
 use Image::ExifTool qw(:Public);
 use Getopt::Std;
+use Data::Dumper;
+use Time::Local;
 
 my $USAGE = <<END;
 USAGE: $0 -i <inputDir> -o <outputDir>
@@ -31,3 +33,29 @@ my @inFiles = (<$inputDir/*>);
 my $inFileCount = scalar @inFiles;
 
 #print "Read $inFileCount inFiles\n";
+
+for my $inFile (@inFiles) {
+	#print "$inFile\n";
+	my $info = ImageInfo($inFile);
+	my $dateTimeOrig = $info->{DateTimeOriginal};
+	my $subSecDateTimeOrig = $info->{SubSecDateTimeOriginal};
+	print "$dateTimeOrig\n";
+	my $epoch = dateTimeToEpoch($dateTimeOrig);
+	my $subSecEpoch = dateTimeToEpoch($subSecDateTimeOrig);
+	print "$dateTimeOrig\t$epoch\n";
+	print "$subSecDateTimeOrig\t$subSecEpoch\n";
+}
+
+sub dateTimeToEpoch
+{
+	# eg: 2013:12:31 22:32:42
+	my $dateTime = shift;
+	my ($date,$time) = split /\s/, $dateTime;
+	my ($year, $month, $day) = split /:/, $date;
+	my ($hour, $minute, $second) = split /:/, $time;
+	#print "dateTime: $dateTime\n";
+	print "year: $year month: $month day: $day hour: $hour minute: $minute second: $second\n";
+	my $epoch = timelocal(0,$minute,$hour,$day,$month-1,$year) + $second;
+	return $epoch;
+}
+
